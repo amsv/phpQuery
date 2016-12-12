55a52952e78be3491fc2b6428ebefbeea6c1b5a6
amsv <ammosovaleksey@ya.ru>
2016-12-13 00:20:10 +0300

    +

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+$url = 'https://privatbank.ua/';
+$file = file_get_contents($url);
+require 'phpQuery.php';
+
+/*$pattern = '#<table id="course-table-pb.+?</table>#s';
+preg_match($pattern, $file, $matches);
+print_r($matches);*/
+
+$doc = phpQuery::newDocument($file);
+//$tbl = $doc->find('#course-table-pb');
+$tr = $doc->find('#selectByPB tr:eq(2) td:eq(1)')->text();
+echo $tr;
+
+?>
